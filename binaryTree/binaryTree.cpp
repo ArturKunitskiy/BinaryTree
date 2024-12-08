de76bea4679164d24859650fb011cc39d79ee6a3
artur artur <arturk102007@gmail.com>
2024-12-08 19:32:02 +0200

    Add project files.

---
+++ binaryTree/binaryTree.cpp
@@ -0,0 +1,46 @@
+#include "Tree.h"
+#include <iostream>
+using namespace std;
+
+Tree::Node::Node(int value, Node* right, Node* left)
+    :value(value),
+    right(right),
+    left(left) {}
+
+void Tree::Insert(Node*& node, int value)
+{
+
+    if (!node)
+    {
+        node = new Node(value);
+        return;
+    }
+    if (value > node->value)
+        Insert(node->right, value);
+    else if (value < node->value)
+        Insert(node->left, value);
+}
+
+void Tree::Print(Node* node) const {
+    if (!node) return;
+    Print(node->left);
+    cout << node->value << " ";
+    Print(node->right);
+}
+
+void Tree::Insert(int value)
+{
+    if (root == nullptr)
+    {
+        root = new Node(value);
+    }
+    else
+    {
+        Insert(root, value);
+    }
+}
+
+void Tree::Print() const {
+    Print(root);
+    cout << endl;
+}
---
+++ binaryTree/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Tree.h"
+using namespace std;
+
+int main()
+{
+	//binary search tree
+	Tree myTree;
+	myTree.Insert(5);
+	myTree.Insert(1);
+	myTree.Insert(8);
+	myTree.Insert(6);
+	myTree.Insert(3);
+	myTree.Print();
+
+	int key;
+	cout << "Input key: ";
+	cin >> key;
+
+	/*if (myTree.Find(key))
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}*/
+
+	myTree.Print();
+}
